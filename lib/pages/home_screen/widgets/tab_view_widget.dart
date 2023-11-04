9eafa1a287b9910faa6530253dc92c19761cd3b0
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2023-11-04 04:14:46 +0200

    Use Precache Image

--- lib/pages/home_screen/widgets/tab_view_widget.dart
+++ lib/pages/home_screen/widgets/tab_view_widget.dart
@@ -4,7 +4,7 @@ import 'package:services_app/common/constants/images.dart';
 
 import '../../../model/models/category_models.dart';
 
-class TabViewWidget extends StatelessWidget {
+class TabViewWidget extends StatefulWidget {
   final Size media;
   final List<CategoryModel> categories;
   const TabViewWidget({
@@ -14,21 +14,36 @@ class TabViewWidget extends StatelessWidget {
   });
 
   @override
+  State<TabViewWidget> createState() => _TabViewWidgetState();
+}
+
+class _TabViewWidgetState extends State<TabViewWidget> {
+  final String cacheImage = Images.emptyImage;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Precache the asset image
+    precacheImage(AssetImage(cacheImage), context);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
-      if (categories.isEmpty) {
+      if (widget.categories.isEmpty) {
         return Padding(
             padding: const EdgeInsets.only(top: 15),
             child: SizedBox(
-              height: media.height * .04,
+              height: widget.media.height * .04,
               child: ListView(
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Image.asset(
-                        Images.emptyImage,
-                        width: media.width * .72,
+                        cacheImage,
+                        width: widget.media.width * .72,
                       ),
                       Padding(
                         padding: const EdgeInsets.only(top: 10),
@@ -36,16 +51,20 @@ class TabViewWidget extends StatelessWidget {
                           "No orders found",
                           style: Fonts.defaultFont.copyWith(
                               fontFamily: "bold-font",
-                              fontSize: media.width * .06),
+                              fontSize: widget.media.width * .06),
                         ),
                       ),
-                      Text(
-                        "you can place your needed orders to let serve you.",
-                        textAlign: TextAlign.center,
-                        style: Fonts.defaultFont.copyWith(
-                            color: const Color(0xff707070),
-                            fontWeight: FontWeight.w400,
-                            fontSize: media.width * .05),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: widget.media.width * .15, vertical: 5),
+                        child: Text(
+                          "you can place your needed orders to let serve you.",
+                          textAlign: TextAlign.center,
+                          style: Fonts.defaultFont.copyWith(
+                              color: const Color(0xff707070),
+                              fontWeight: FontWeight.w400,
+                              fontSize: widget.media.width * .05),
+                        ),
                       ),
                     ],
                   )
@@ -56,13 +75,13 @@ class TabViewWidget extends StatelessWidget {
         return ListView.builder(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
-          itemCount: categories.length,
+          itemCount: widget.categories.length,
           itemBuilder: (context, index) => Padding(
             padding: EdgeInsets.symmetric(
-                vertical: 10, horizontal: media.width * .04),
+                vertical: 10, horizontal: widget.media.width * .04),
             child: Container(
                 padding: EdgeInsets.symmetric(
-                    horizontal: 15, vertical: media.height * .005),
+                    horizontal: 15, vertical: widget.media.height * .005),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
                   color: Colors.white,
@@ -77,16 +96,16 @@ class TabViewWidget extends StatelessWidget {
                 ),
                 child: Row(children: [
                   Text(
-                    "${categories[index].id}. ",
+                    "${widget.categories[index].id}. ",
                     style: Fonts.defaultFont,
                   ),
                   const SizedBox(
                     width: 15,
                   ),
                   Text(
-                    "${categories[index].name}. ",
+                    "${widget.categories[index].name}. ",
                     style: Fonts.defaultFont.copyWith(
-                      fontSize: media.width * .04,
+                      fontSize: widget.media.width * .04,
                       fontWeight: FontWeight.w400,
                     ),
                   ),
